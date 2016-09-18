25aa52c1998925e0a3151d5b56657db2fa850f67
DeveloperTommy <its.tommy.nguyen@gmail.com>
2016-09-18 12:16:51 -0400

    Implemented rebase and squash with last 5 commits

--- test/BasicStatsTest.java
+++ test/BasicStatsTest.java
@@ -17,11 +17,54 @@ public class BasicStatsTest {
 
     @Test
     public void testMedian() {
+      //Median should be 8.0 since size is even
+      /* double[] numbers = {1, 4, 7, 9, 11, 21}; */
+      double[] numbers = {9, 11, 1, 4, 7, 21};
 
+      double median = BasicStats.median(numbers);
+      assertEquals(8.0, median, EPS);
+
+      //Median should be 7 since size is odd
+      double[] numbers2 = {9, 1, 4, 7, 21};
+      median = BasicStats.median(numbers2);
+      assertEquals(7, median, EPS);
+
+      //Median should be 3 since size is 1
+      double[] numbers3 = {3};
+      median = BasicStats.median(numbers3);
+      assertEquals(3, median, EPS);
+
+      //Median should be 0 since size is 0
+      double[] numbers4 = {};
+      median = BasicStats.median(numbers4);
+      assertEquals(0, median, EPS);
     }
 
     @Test
     public void testMode() {
-      
+      //Mode should be 1 since we are taking the first mode
+      double[] numbers = {1, 4, 7, 9, 11, 21};
+      double mode   = BasicStats.mode(numbers);
+      assertEquals (1, mode, EPS);
+
+      //Mode should be 4
+      double[] numbers2 = {1, 4, 4, 7, 9, 11, 21};
+      mode   = BasicStats.mode(numbers2);
+      assertEquals (4, mode, EPS);
+
+      //Mode should be 7
+      double[] numbers3 = {4, 4, 7, 7, 7, 11, 11};
+      mode   = BasicStats.mode(numbers3);
+      assertEquals (7, mode, EPS);
+
+      //Mode should be 7
+      double[] numbers4 = {7};
+      mode   = BasicStats.mode(numbers4);
+      assertEquals (7, mode, EPS);
+
+      //Mode should be 0
+      double[] numbers5 = {};
+      mode   = BasicStats.mode(numbers5);
+      assertEquals (0, mode, EPS);
     }
 }
